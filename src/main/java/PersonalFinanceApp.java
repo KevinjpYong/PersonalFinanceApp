d55195246cb4da1a1ad701cd7c1ff19d71b79a2e
yongk1 <kevinjp.yong@gmail.com>
2023-03-24 14:48:45 +1100

    setup workflow for Github actions

---
+++ src/main/java/PersonalFinanceApp.java
@@ -0,0 +1,6 @@
+public class PersonalFinanceApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/PersonalFinanceAppTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PersonalFinanceAppTests {
+
+    @Test
+    public void firstTest(){
+        assertEquals(true,true);
+    }
+}
